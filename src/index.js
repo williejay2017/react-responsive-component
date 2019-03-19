10cbd48b01066fcb78a678f768cf29b00177f4ac
Joshua Wilford <williejay2009@gmail.com>
2019-03-19 00:38:08 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import styles from './responsive.css';
 
-class ResponsiveMedia extends Component {
+ResponsiveMedia.propTypes = {
+    size: PropTypes.string.isRequired
+};
+
+export class ResponsiveMedia extends Component {
 render() {
     return (
     <div className={`ResponsiveMedia-${this.props.size || 'desktop'}`}>TESTING
@@ -11,8 +15,5 @@ render() {
     );
   }
 };
-ResponsiveMedia.propTypes = {
-    size: PropTypes.string.isRequired
-};
+
  
-export default ResponsiveMedia;
